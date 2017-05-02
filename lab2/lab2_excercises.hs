c25bddbc3667aaf803388726037e2d65c5495026
Tobias Axelsson <Tobias Axelsson>
2017-05-02 16:05:26 +0200

    jobbat på med excercises lab 2

--- lab2/lab2_excercises.hs
+++ lab2/lab2_excercises.hs
@@ -29,3 +29,41 @@ semiColon2 = lit1 ';'
 
 twoChars1 :: Parser (Char, Char)
 twoChars1 = char1 # char1
+
+toUpper1 :: Parser Char
+toUpper1 = letter1 >-> toUpper
+
+sndChar1 :: Parser Char
+sndChar1 = twoChars1 >-> snd
+
+twoChars2 :: Parser String
+twoChars2 = twoChars1 >-> tupleToString
+	where
+	tupleToString :: (Char, Char) -> String
+	tupleToString (a, b) = [a] ++ [b]
+
+--operators
+(-#) :: Parser a -> Parser b -> Parser b
+(m -# n) cs = (m # n >-> snd) cs
+
+(#-) :: Parser a -> Parser b -> Parser a
+(m #- n) cs = (m # n >-> fst) cs
+
+{-(!) :: Parser a -> Parser a -> Parser a
+(m ! n) cs = case m cs of
+             Nothing -> n cs 
+             mcs -> mcs-}
+
+iterate1 :: Parser a -> Int -> Parser [a]
+iterate1 m 0 = CoreParser.return []
+iterate1 m i = m # iterate1 m (i - 1) >-> cons1
+
+--constructs a list from a parser tuple.
+cons1 :: (a, [a]) -> [a]
+cons1 (hd, tl) = hd:tl
+
+iterate2 :: Parser a -> Parser [a]
+iterate2 m = (((m # (iterate2 m))) >-> cons1) ! (CoreParser.return [])
+
+--token :: Parser a -> Parser a
+--token m = m #
